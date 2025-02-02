746f3b776798f1bed738908a37f9d9a5996bac5e
brandonacuna1 <brandonacuna507@gmail.com>
2025-02-01 22:56:42 -0600

    prueba corta
    esta es la prueba de lo que hice y da resultados

---
+++ Python/prueba corta/prueba python.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Sensores (
+    ID_Sensor INT identity PRIMARY KEY, 
+    temperatura varchar (10));
+
+	select * from Sensores
+
+	insert into Sensores(temperatura)
+	values
+	('11'),('13'),('15'),('17'),('20'),('24'),('27');
+
+
+	select * from sensores
